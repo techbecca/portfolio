7d4d8bf62a07dc0f2617779ccba1c4fd07c4d0b0
rebecca <rebecca@kry.se>
2019-06-04 09:08:32 +0200

    Update CSS

--- index.php
+++ index.php
@@ -13,25 +13,27 @@
 </head>
 
 <body>
-  <div class="header">
+  <div class="header background-red white">
     <br/>
     <br/>
     <h1>Techbecca's Portfolio</h1>
     <h3>View my shit. Hopefully it's awesome.</h3>
     <br/>
   </div>
+  <h1> Gallery </h1>
   <div class="albums">
-    <h1> Gallery </h1>
     <?php
       $path = "./resources/img/albums";
       $it = new FilesystemIterator($path, FilesystemIterator::SKIP_DOTS);
       foreach ($it as $filepath => $fileinfo) {
         if ($fileinfo->isDir()) {
           echo "<h2>".$fileinfo->getFilename()."</h2>";
+          echo "<div class=\"container\">";
           $imgIt = new FilesystemIterator($filepath, FilesystemIterator::KEY_AS_PATHNAME);
           foreach ($imgIt as $imgFileinfo) {
             echo "<img src=\"".$imgIt->key()."\" >";
           }
+          echo "</div>";
         }
       }
     ?>
