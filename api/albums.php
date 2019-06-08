730ba130ff6e690216cdad123bddaf9cee287fd1
rebecca <rebecca@kry.se>
2019-06-08 21:49:04 +0200

    GET albums file

--- api/albums.php
+++ api/albums.php
@@ -1,6 +1,32 @@
 <?php
+  // required headers
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
 
-  function get_albums($path = "./resources/img/albums";) {
+  function albums()
+  {
+    $list = array();
+    $list = get_albums("./resources/img/albums");
+    if (sizeof($list)>0)
+    {
+      // Set response code - 200 OK
+      http_response_code(200);
+
+      // Show albums in json formats
+      echo json_encode($list);
+
+    }
+    else {
+      // Set response code - 404 Not found
+      http_response_code(404);
+
+      // Set error message
+      echo json_encode($temp = array('message' => "No albums found");)
+    }
+  }
+
+  // Get albums data from folders
+  private function get_albums($path = "./resources/img/albums";) {
     var $albums = array();
     var $images;
     var $name;
@@ -12,12 +38,17 @@
         // Collect name of album
         $name = $fileinfo->getFilename();
         // Collect the images
-        $images = array_diff(scandir($path), array('.', '..'));
+        $images = array();
+        $imgIt = new FilesystemIterator($filepath, FilesystemIterator::KEY_AS_PATHNAME);
+        foreach ($imgIt as $imgFileinfo) {
+          //TODO: test that we actually have an image before displaying
+          array_push($images, $imgIt->key());
+        }
         // TODO: use glob() to only get valid img formats
         // Enter a key-value pair with name and a scan of the directory
         $albums[$name]=$images;
       }
-      var_dump($albums);
     }
+    return $albums;
   }
  ?>
