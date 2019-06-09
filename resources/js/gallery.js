c4b9f94c67e2265e6fdf7355e088fd14db882d62
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2019-06-09 20:47:54 +0200

    Add request for albums JSON and print status in gallery.js

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -5,20 +5,16 @@ $(document).ready(function() {
       <p> Hello world </p>
     `;
 
-  // app html
-  var app_html = `
-        <div class='container'>
-
-            <div class='page-header'>
-                <h1 id='page-title'>Read Products</h1>
-            </div>
-
-            <!-- this is where the contents will be shown. -->
-            <div id='page-content'></div>
-
-        </div>`;
-
   // inject to 'gallery' in index.html
   $("#gallery").html(gallery_html);
 
+  showAlbums();
+
 });
+
+function showAlbums(){
+  $.get("./api/albums.php", function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+  });
+
+}
