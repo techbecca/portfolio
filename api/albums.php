f36df6b8c8cb7a5cced290c603047e5a8e26fd87
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2019-06-09 20:05:14 +0200

    Revert "Move album to a separate file"
    This reverts commit 7c147d0e91191fcd0d8e54adc61c717dc76edc72.

--- api/albums.php
+++ api/albums.php
@@ -1,5 +1,4 @@
 <?php
-  include "../classes/album.php";
   // required headers
   header("Access-Control-Allow-Origin: *");
   header("Content-Type: application/json; charset=UTF-8");
@@ -31,4 +30,44 @@
     // Set error message
     echo json_encode(array('message' => "No albums found"));
   }
+
+  /**
+   *
+   */
+  private class Album
+  {
+    var $name;
+    var $path;
+    var $images;
+    var $empty = TRUE;
+
+    function __construct($name, $path)
+    {
+      $this->$name = $name;
+      $this->$path = $path;
+      $this->$images = get_images($path);
+      if (sizeof($images)>0) {
+        $this->$empty = FALSE;
+      }
+    }
+
+    function get_images($path="") {
+      $images = array();
+      if ($path="" && is_string($path)) {
+        throw new \Exception("No path was given", 1);
+      } else {
+        // Collect the images
+        // TODO: use glob() to only get valid img formats
+        $imgIt = new FilesystemIterator($path, FilesystemIterator::KEY_AS_PATHNAME);
+        foreach ($imgIt as $imgFileinfo) {
+          //TODO: test that we actually have an image before displaying
+          array_push($images, $imgIt->key());
+        }
+      }
+      return $images;
+    }
+  }
+
+
+
  ?>
