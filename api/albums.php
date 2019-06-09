bcdcb2c68303d8b6e79bb9c46105668be7ffe5c6
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2019-06-09 19:40:51 +0200

    Refactor with album class

--- api/albums.php
+++ api/albums.php
@@ -9,19 +9,13 @@
   foreach ($it as $filepath => $fileinfo) {
     // If it's an album and not a file
     if ($fileinfo->isDir()) {
-      // Collect name of album
-      $name = $fileinfo->getFilename();
-      // Collect the images
-      $images = array();
-      // TODO: use glob() to only get valid img formats
-      $imgIt = new FilesystemIterator($filepath, FilesystemIterator::KEY_AS_PATHNAME);
-      foreach ($imgIt as $imgFileinfo) {
-        //TODO: test that we actually have an image before displaying
-        array_push($images, $imgIt->key());
+      // Create album from directory
+      $album = new Album($fileinfo->getFilename(), $filepath);
+      // If album isn't empty
+      if ($album->$empty === FALSE) {
+        // Add to list of albums
+        $albums[]=$album;
       }
-
-      // Enter a key-value pair with name and a scan of the directory
-      $albums[$name]=$images;
     }
   }
   // Check if images are there
@@ -40,6 +34,44 @@
     // Set error message
     echo json_encode(array('message' => "No albums found"));
   }
+
+  /**
+   *
+   */
+  private class Album
+  {
+    var $name;
+    var $path;
+    var $images;
+    var $empty = TRUE;
+
+    function __construct($name, $path)
+    {
+      $this->$name = $name;
+      $this->$path = $path;
+      $this->$images = get_images($path);
+      if (sizeof($images)>0) {
+        $this->$empty = FALSE;
+      }
+    }
+
+    function get_images($path="") {
+      $images = array();
+      if ($path="" && is_string($path)) {
+        throw new \Exception("No path was given", 1);
+      } else {
+        // Collect the images
+        // TODO: use glob() to only get valid img formats
+        $imgIt = new FilesystemIterator($path, FilesystemIterator::KEY_AS_PATHNAME);
+        foreach ($imgIt as $imgFileinfo) {
+          //TODO: test that we actually have an image before displaying
+          array_push($images, $imgIt->key());
+        }
+      }
+      return $images;
+    }
+  }
+
 
 
  ?>
