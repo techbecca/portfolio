07f9cd1ff482016890cd79074e8c153b1d04ee2e
rebecca <rebecca@kry.se>
2019-06-12 12:33:59 +0200

    Revert album from id to class

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -8,15 +8,14 @@ function showAlbums(){
   $.getJSON("./api/albums.php", function(albums, status){
     // Loop through albums, for each album:
     for (var key in albums) {
-      console.log(key);
       // Create element for html heading
       // Append heading to albums div
-      $('<h2>'+key+'</h2>').appendTo("#albums");
+      $('<h2>'+key+'</h2>').appendTo(".albums");
       // Add container element with album name as id
       var container = $(document.createElement('div'));
       container.attr('class', 'container');
       container.attr('id', key);
-      container.appendTo("#albums")
+      container.appendTo(".albums")
       // Get array of images
       var images = albums[key];
       // For each image:
