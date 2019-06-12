3c1110524c7c5af89ca0daa82e21782611e320c1
rebecca <rebecca@kry.se>
2019-06-12 08:34:41 +0200

    Generate html for images dynamically

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -14,9 +14,21 @@ $(document).ready(function() {
 
 function showAlbums(){
   $.getJSON("./api/albums.php", function(albums, status){
-    console.log(albums);
+    // Loop through albums, for each album:
     for (var key in albums) {
       console.log(key);
+      // Create element for html heading
+      // Append heading to albums div
+      $('<h2>'+key+'</h2>').appendTo("#albums");
+      // For each image:
+      for (imgSrc of images[album]) {
+        // Create image element
+        var img = $(document.createElement('img'));
+        // Add source attribute
+        img.attr('src', imgSrc);
+        // Append image to albums div
+        img.appendTo("#albums");
+      }
     }
   });
 
