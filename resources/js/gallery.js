f0bd966f2cbb2f29956fcbc6f938706dda210b8f
rebecca <rebecca@kry.se>
2019-06-18 20:23:49 +0200

    Revert "Sixth modal attempt"
    This reverts commit 671178ad3f97ea77da1e7e93b0a35f51ff41f4ea.

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -21,33 +21,16 @@ $(document).ready(function() {
         });
 
         // Create modal
-
-        // <!-- The Modal -->
-        // <div id="myModal" class="modal">
-        //   <!-- The Close Button -->
-        //   <span class="close">&times;</span>
-        //   <!-- Modal Content (The Image) -->
-        //   <img class="modal-content">
-        //   <!-- Modal Caption (Image Text) -->
-        //   <div id="caption"></div>
-        // </div>
-        modalId = "modal-"+imgId;
-        var modal = $('div').attr('class', 'modal').attr('id', modalId);
-        modal.append($('span').text('&times;').attr('close'));
-        modal.append($('img').attr({
-          class: "modal-content",
-          src: imgSrc,
-          alt: imgId
-        }));
+        var modal = $("#myModal").clone().removeAttr('id');
         img.click(function() {
-          $('#'+modalId).children('.modal-content').attr('src', $(this).src);
-          $('#'+modalId).show();
+          modal.show();
+          modal.children('img').attr('src', imgSrc);
         });
 
         // When you click any close Button
-        $('#'+modalId).children(".close").click(function() {
+        modal.find(".close").click(function() {
           // Hides all modals
-          $('#'+modalId).hide();
+          modal.hide();
         })
 
         // Append image & modal to albumContainer div
