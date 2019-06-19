5438d205e40b9d67aa3cc70276eec0b71a1e3f2c
rebecca <rebecca@kry.se>
2019-06-19 12:45:15 +0200

    Functioning modal!

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -1,27 +1,31 @@
+var production = true;
+var environment = production ? "." : "http://www.techbecca.me";
 $(document).ready(function() {
 
-  $.getJSON("./api/albums.php", function(albums, status) {
+  $.getJSON(environment + "/api/albums.php", function(albums, status) {
     // Loop through albums, for each album:
     for (var albumName in albums) {
       // Create element for html heading & Append heading to albums div
       $('<h2>' + albumName + '</h2>').appendTo(".albums");
-      // Add albumContainer element with album name as id
-      var albumContainer = $('<div>');
-      albumContainer.attr('class', 'container');
+      // Add container element with album name as id
+      var container = $('<div>');
+      container.attr('class', 'container');
       // For each image:
       let i = 0;
       for (imgSrc of albums[albumName]) {
         // Create id
         let imgId = albumName + "-" + i++;
-
+        imgSrc = environment + imgSrc.substring(2);
         // Create image element & Add source attribute
         var img = $('<img />', {
           src: imgSrc,
-          id: imgId
+          id: imgId,
+          alt: imgId
         });
         img.click(function() {
           $("#myModal").show();
-          $("#img01").attr('src', imgSrc);
+          $("#img01").attr('src', $(this).attr('src'));
+          $("#caption").text($(this).attr('alt'));
         });
         // Append image to container div
         container.append(img);
@@ -30,22 +34,6 @@ $(document).ready(function() {
       container.appendTo(".albums");
     }
 
-    $.each($("div.albums").children('img'), function(index, image){
-      console.log("Index: " + index + " Src: " + image.src);
-      let modalId = '#modal-' + image.id;
-      let modalContentId = '#modal-content-'+image.id;
-      // Add click-on img to open modal
-      image.click(function() {
-        $(modalId).show();
-        $(modalContentId).attr('src', $(this).src);
-      });
-
-      // When you click the close button
-      $()(".close").click(function() {
-        // Hides the modal
-        $(modalId).hide();
-      })
-    });
 
   });
 
